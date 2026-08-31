5bd3e003daf1d5e27366401468fb82226e1b90db
David <javidoru02@gmail.com>
2026-08-31 22:56:19 +0300

    make new tables

--- fileSQL/internet_shop.sql
+++ fileSQL/internet_shop.sql
@@ -1,57 +1,63 @@
--- CREATE TABLE product_type(
---     id SERIAL PRIMARY KEY,
---     type_name VARCHAR(100) NOT NULL
--- );
+CREATE TABLE product_type(
+    id SERIAL PRIMARY KEY,
+    type_name VARCHAR(100) NOT NULL
+);
 
---  CREATE TABLE product_categories(
---     id SERIAL PRIMARY KEY,
---     categories_name VARCHAR(100) NOT NULL
---  );
+CREATE TABLE product_category(
+    id SERIAL PRIMARY KEY,
+    category_name VARCHAR(100) NOT NULL
+);
 
---  CREATE TABLE product_brand(
---     id SERIAL PRIMARY KEY,
---     brand_name VARCHAR(100) NOT NULL
---  );
+CREATE TABLE product_brand(
+    id SERIAL PRIMARY KEY,
+    brand_name VARCHAR(100) NOT NULL
+);
 
---   CREATE TABLE product_model(
---     id SERIAL PRIMARY KEY,
---     model_name VARCHAR(100) NOT NULL
---  );
+CREATE TABLE product_model(
+    id SERIAL PRIMARY KEY,
+    brand_id INTEGER REFERENCES product_brand(id) NOT NULL,
+    model_name VARCHAR(100) NOT NULL
+);
 
--- CREATE TABLE client(
---      id SERIAL PRIMARY KEY,
---      name VARCHAR(100) NOT NULL,
---      email VARCHAR(100) NOT NULL
--- );
+CREATE TABLE client(
+      id SERIAL PRIMARY KEY,
+      client_name VARCHAR(100) NOT NULL,
+      email VARCHAR(100) NOT NULL
+ );
 
+CREATE TABLE product(
+    id SERIAL PRIMARY KEY,
+    product_name VARCHAR(100) NOT NULL,
+    description VARCHAR(100),
+    price DECIMAL(10, 2) NOT NULL,
+    type_id INTEGER REFERENCES product_type(id) NOT NULL,
+    category_id INTEGER REFERENCES product_category(id) NOT NULL,
+    model_id INTEGER REFERENCES product_model(id) NOT NULL
+ );
 
--- CREATE TABLE product(
---     id SERIAL PRIMARY KEY,
---     name VARCHAR(100) NOT NULL,
---     description TEXT,
---     price DECIMAL(10, 2),
---     typeId INTEGER REFERENCES product_type(id) NOT NULL,
---     categoryId INTEGER REFERENCES product_categories(id) NOT NULL,
---     brandId INTEGER REFERENCES product_brand(id) NOT NULL,
---     modelId INTEGER REFERENCES product_model(id) NOT NULL
--- );
+CREATE TABLE storage(
+    id SERIAL PRIMARY KEY,
+    city VARCHAR(100) NOT NULL,
+    storage_name VARCHAR(100) NOT NULL
+ );
 
--- CREATE TABLE storage(
---     id SERIAL PRIMARY KEY,
---     city VARCHAR(100) NOT NULL,
---     storage_name VARCHAR(100) NOT NULL
--- );
+CREATE TABLE storage_product(
+    id SERIAL PRIMARY KEY,
+    product_id INTEGER REFERENCES product(id) NOT NULL,
+    storage_id INTEGER REFERENCES storage(id) NOT NULL,
+    quantity INTEGER NOT NULL
+);
 
--- CREATE TABLE storage_product(
---     id SERIAL PRIMARY KEY,
---     productId INTEGER REFERENCES product(id) NOT NULL,
---     storageId INTEGER REFERENCES storage(id) NOT NULL,
---     quantity INTEGER NOT NULL    
--- );
+CREATE TABLE orders(
+    id SERIAL PRIMARY KEY,
+    client_id INTEGER REFERENCES client(id) NOT NULL,
+    order_date DATE NOT NULL,
+    is_paid BOOLEAN NOT NULL
+);
 
--- CREATE TABLE orders(
---     id SERIAL PRIMARY KEY,
---     productId INTEGER REFERENCES product(id) NOT NULL,
---     clientId INTEGER REFERENCES client(id) NOT NULL
-
--- );
+CREATE TABLE orders_product(
+    id SERIAL PRIMARY KEY,
+    product_id INTEGER REFERENCES product(id) NOT NULL,
+    order_id INTEGER REFERENCES orders(id) NOT NULL,
+    quantity INTEGER NOT NULL
+);
