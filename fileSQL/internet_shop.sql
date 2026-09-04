d9a737ad1a514b60c4eb6681ff52589d64fb1deb
David <javidoru02@gmail.com>
2026-09-05 01:55:06 +0300

    i hope final commit

--- fileSQL/internet_shop.sql
+++ fileSQL/internet_shop.sql
@@ -1,22 +1,26 @@
 CREATE TABLE product_type(
     id SERIAL PRIMARY KEY,
-    type_name VARCHAR(100) NOT NULL
+    type_name VARCHAR(100) NOT NULL,
+    description TEXT
 );
 
 CREATE TABLE product_category(
     id SERIAL PRIMARY KEY,
-    category_name VARCHAR(100) NOT NULL
+    category_name VARCHAR(100) NOT NULL,
+    description TEXT
 );
 
 CREATE TABLE product_brand(
     id SERIAL PRIMARY KEY,
-    brand_name VARCHAR(100) NOT NULL
+    brand_name VARCHAR(100) NOT NULL,
+    description TEXT
 );
 
 CREATE TABLE product_model(
     id SERIAL PRIMARY KEY,
     brand_id INTEGER REFERENCES product_brand(id) NOT NULL,
-    model_name VARCHAR(100) NOT NULL
+    model_name VARCHAR(100) NOT NULL,
+    description TEXT
 );
 
 CREATE TABLE client(
@@ -27,8 +31,7 @@ CREATE TABLE client(
 
 CREATE TABLE product(
     id SERIAL PRIMARY KEY,
-    product_name VARCHAR(100) NOT NULL,
-    description VARCHAR(100),
+    description TEXT,
     price DECIMAL(10, 2) NOT NULL,
     type_id INTEGER REFERENCES product_type(id) NOT NULL,
     category_id INTEGER REFERENCES product_category(id) NOT NULL,
@@ -38,23 +41,24 @@ CREATE TABLE product(
 CREATE TABLE storage(
     id SERIAL PRIMARY KEY,
     city VARCHAR(100) NOT NULL,
-    storage_name VARCHAR(100) NOT NULL
+    storage_name VARCHAR(100) NOT NULL,
+    quantity INTEGER NOT NULL
  );
 
 CREATE TABLE storage_product(
-    id SERIAL PRIMARY KEY,
     product_id INTEGER REFERENCES product(id) NOT NULL,
-    storage_id INTEGER REFERENCES storage(id) NOT NULL,
-    quantity INTEGER NOT NULL
+    storage_id INTEGER REFERENCES storage(id) NOT NULL
 );
 
 CREATE TABLE orders(
     id SERIAL PRIMARY KEY,
     client_id INTEGER REFERENCES client(id) NOT NULL,
-
+    description TEXT,
+    date DATE NOT NULL,
+    is_paid BOOLEAN NOT NULL
 );
 
 CREATE TABLE orders_product(
     product_id INTEGER REFERENCES product(id) NOT NULL,
-    order_id INTEGER REFERENCES orders(id) NOT NULL,
+    order_id INTEGER REFERENCES orders(id) NOT NULL
 )
