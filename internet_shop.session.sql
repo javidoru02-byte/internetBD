879ea00e71c6b5147daa037d4c00333bd660f38e
David <javidoru02@gmail.com>
2026-09-11 02:31:32 +0300

    delate wrong file

--- internet_shop.session.sql
+++
@@ -1,6 +0,0 @@
-SELECT client.client_name, COUNT(orders.id) AS order_count
-FROM orders
-JOIN client ON orders.client_id = client.id
-GROUP BY client.client_name
-ORDER BY order_count DESC
-LIMIT 1;
