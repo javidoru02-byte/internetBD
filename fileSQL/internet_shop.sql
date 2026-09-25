7edf5a4d557dff23ac0506db9bf8cb521d0c40cc
David <javidoru02@gmail.com>
2026-09-25 19:08:39 +0300

    script 2 now show sum of all product

--- fileSQL/internet_shop.sql
+++ fileSQL/internet_shop.sql
@@ -1,11 +1,17 @@
+скрипт 1
+
 SELECT COUNT(DISTINCT product_model.id) AS model_count, product_brand.brand_name AS brand_name
 FROM product_model
 JOIN product_brand ON product_model.brand_id = product_brand.id
 GROUP BY brand_id, product_brand.brand_name;
 ---------------------
-SELECT storage_name, quantity AS total_quantity
+скрипт 2
+
+SELECT SUM(quantity) AS total_all_products
 FROM storage;
----------------------
+---------------------ґ
+скрипт 3
+
 SELECT client.client_name, COUNT(orders.id) AS order_count
 FROM orders
 JOIN client ON orders.client_id = client.id
@@ -13,11 +19,13 @@ GROUP BY client.client_name
 ORDER BY order_count DESC
 LIMIT 1;
 ----------------------
-SELECT client.client_name, SUM(product.price) AS bigest_order_value
+скрипт 4
+
+SELECT client.client_name, MAX(product.price) AS bigest_order_value
 FROM client
 JOIN orders ON client.id = orders.client_id
 JOIN orders_product ON orders.id = orders_product.order_id
 JOIN product ON orders_product.product_id = product.id
-GROUP BY client.client_nameS
+GROUP BY client.client_name
 ORDER BY bigest_order_value DESC
 LIMIT 1;
